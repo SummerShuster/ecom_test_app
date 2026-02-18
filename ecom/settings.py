fa15d3416de645c57eb9c7bfc7d29151d6e1633d
Summer Shuster <shustersummer@gmail.com>
2026-02-18 15:14:17 -0600

    added DB Password environ

--- ecom/settings.py
+++ ecom/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "PASSWORD": os.environ["DB_PASSWORD_YO"],
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
         "OPTIONS": {"sslmode": "require"},
