c3d4aa611751c7063fd20fd035189c30c8b7519f
Summer Shuster <shustersummer@gmail.com>
2026-02-19 14:30:46 -0600

    more domain name stuff

--- ecom/settings.py
+++ ecom/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['https://summerlilythrift.com', 'web-production-4002.up.railway.app', 'https://web-production-4002.up.railway.app']
+ALLOWED_HOSTS = ['https://summerlilythrift.com','summerlilythrift.com', 'web-production-4002.up.railway.app', 'https://web-production-4002.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://summerlilythrift.com', 'https://web-production-4002.up.railway.app']
 
 
